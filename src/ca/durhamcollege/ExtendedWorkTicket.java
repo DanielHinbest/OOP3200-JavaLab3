a3451a57cc7fa61a3bd15805690e1b8ae8734b80
Daniel Hinbest <daniel.hinbest@dcmail.ca>
2020-12-03 16:02:24 -0500

    ExtendedWorkTicket class done

--- src/ca/durhamcollege/ExtendedWorkTicket.java
+++ src/ca/durhamcollege/ExtendedWorkTicket.java
@@ -3,17 +3,61 @@ package ca.durhamcollege;
 public class ExtendedWorkTicket extends WorkTicket
 {
     //Declaration
-    boolean isOpen;
+    boolean myOpen;
 
+    //ACCESSOR
+    public boolean isOpen()
+    {
+        return myOpen;
+    }
+
+    //MUTATORS
+    public void openTicket()
+    {
+        myOpen = true;
+    }
+    public void closeTicket()
+    {
+        myOpen = false;
+    }
+    public boolean setWorkTicket(int ticketNumber, String clientId, int year, int month, int day, String issueDescription,
+                                 boolean myOpen)
+    {
+        boolean isValid = true;
+
+        if (myOpen != true)
+        {
+            isValid = false;
+        }
+        else {
+            super.setWorkTicket(ticketNumber, clientId, year, month, day, issueDescription);
+            this.myOpen = myOpen;
+        }
+        return isValid;
+    }
+
+    @Override
+    public String toString() {
+        String message = super.toString();
+
+        if (isOpen() == true) {
+            return super.toString() + "Ticket: OPEN";
+        }
+        else {
+            return super.toString() + "Ticket: CLOSED";
+        }
+    }
+
+    //Constructors
     ExtendedWorkTicket(){
         super();
-        isOpen = true;
+        myOpen = true;
     }
 
     ExtendedWorkTicket(int ticketNumber, String clientID, int year, int month, int day, String issueDescription,
-                       boolean isOpen)
+                       boolean myOpen)
     {
         super(ticketNumber, clientID, year, month, day, issueDescription);
-        this.isOpen = isOpen;
+        this.myOpen = myOpen;
     }
 }
